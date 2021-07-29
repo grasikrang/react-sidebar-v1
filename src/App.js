14a22f471bad11e3205f53f1d90ca982255ad316
grasikrang <grasikrang@gmail.com>
2021-07-29 19:07:37 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar></Navbar>
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/products" component={Products}/>
+            <Route path="/reports" component={Reports}/>
+          </Switch>
+      </Router>      
+    </>
+  );
+}
+
+export default App;
